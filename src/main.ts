4628dae512a9ea50cbe25b42c093e3883b1a2b60
Sanjeev Kumar Singh <sanjeev.singh@napses.com>
2024-07-30 20:49:12 +0530

    added global middleware for testing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NextFunction, Request, Response } from 'express';
+
+function globalMiddleWareOne(req: Request, res: Response, next: NextFunction) {
+  console.log('this is global middleware 1');
+  next();
+}
+
+function globalMiddleWareTwo(req: Request, res: Response, next: NextFunction) {
+  console.log('this is global middleware 2');
+  next();
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(globalMiddleWareOne);
+  app.use(globalMiddleWareTwo);
   await app.listen(4000);
 }
 bootstrap();
